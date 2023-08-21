7482bfd68ef130afcf35fb4b2bac6600c16bcefc
leauven <mertarasofficial@gmail.com>
2023-08-22 01:35:46 +0300

    Last Commit & Update

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Daily To-Do List Project
+
+This project is a simple to-do list application that I created for personal use while learning JavaScript.
+
+## How to Use
+
+1. Download the project files.
+2. Open the `index.html` file in your web browser.
+
+Feel free to use and modify the project according to your needs. If you have any questions or suggestions, please feel free to reach out.
--- script.js
+++ script.js
@@ -10,7 +10,10 @@ function addTask() {
 
   const li = document.createElement('li');
   li.textContent = taskText;
-  
+
+  // Add 'click' event listener to each list item
+  li.addEventListener('click', toggleTextColor);
+
   const deleteButton = document.createElement('button');
   deleteButton.textContent = 'X';
   deleteButton.classList.add('delete-button');
@@ -22,7 +25,48 @@ function addTask() {
   taskInput.value = '';
 }
 
+function toggleTextColor(event) {
+  const listItem = event.target;
+  const currentColor = window.getComputedStyle(listItem).color;
+  const newColor = currentColor === 'rgb(0, 0, 0)' ? 'gray' : 'black';
+  listItem.style.color = newColor;
+  if (newColor === 'gray') {
+    listItem.style.textDecoration = 'line-through';
+  } else {
+    listItem.style.textDecoration = 'none';
+  }
+}
+
 function deleteTask(event) {
   const listItem = event.target.parentElement;
   taskList.removeChild(listItem);
 }
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
